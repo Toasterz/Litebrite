2b94248041554b27610417ed3b8b638182d7186b
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-06-27 20:58:48 -0400

    got grid displaying

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
 $(document).ready(function()
-{ 
+{
+  var container = $('.container');
+  var numRows = 10;
+  var numCols = 10;
+
+  initGrid();
+  function initGrid()
+  {
+    for (var i = 0; i < numRows; i+=1)
+    {
+      var row = $('<div></div>');
+      for (var j = 0; j < numCols; j+=1)
+      {
+        var cell = $('<div></div>');
+        cell.addClass('cell border');
+        row.append(cell);
+      }
+      container.append(row);
+    }
+  }
 });
